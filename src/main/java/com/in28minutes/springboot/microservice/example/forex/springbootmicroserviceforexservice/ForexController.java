5c968101bbe8395b0ba062834f80564526989427
ROSSAMYYOUSSEF <youssef.rsy@gmail.com>
2018-07-23 17:10:49 +0000

    adding project to git

---
+++ src/main/java/com/in28minutes/springboot/microservice/example/forex/springbootmicroserviceforexservice/ForexController.java
@@ -0,0 +1,40 @@
+package com.in28minutes.springboot.microservice.example.forex.springbootmicroserviceforexservice;
+
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+public class ForexController {
+	
+		@Autowired
+	  private Environment environment;
+
+	  
+
+		@Autowired
+	  private ExchangeValueRepository repository;
+
+	  
+
+	  @GetMapping("/currency-exchange/from/{from}/to/{to}")
+	  public ExchangeValue retrieveExchangeValue
+	    (@PathVariable String from, @PathVariable String to){
+
+	    
+
+	    ExchangeValue exchangeValue = 
+
+	        repository.findByFromAndTo(from, to);
+
+	    
+
+	    exchangeValue.setPort(
+
+	        Integer.parseInt( environment.getProperty("local.server.port")));
+
+
+	    return exchangeValue;
+
+	  }
+}
